d14a6f8cf46e9289266c5a3e20ec41f54fd21c70
Mark McClain <mark@mcclain.xyz>
2015-07-13 23:09:26 +0000

    Add license headers

--- dhc_nsx/ml2/extension_driver.py
+++ dhc_nsx/ml2/extension_driver.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2015 Akanda Inc
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from neutron.api.v2 import attributes as attr
 from neutron.db import portsecurity_db
 from neutron.extensions import allowedaddresspairs as addr_pair
